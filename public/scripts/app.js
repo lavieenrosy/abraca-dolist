26dbe1eb35d6a621aa69c118210795b802d14e49
Rosy Lee <rosyhlee@gmail.com>
2018-10-18 23:07:13 +0000

    wrote createTodoElement and renderTodos functions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,22 +9,30 @@ $(() => {
     }
   });;
 
-  // <div class="row">
-
-  //       <!-- Eat list -->
-
-  //       <div class="col">
-
-  //         <div class="card">
-  //           <div class="card-body">
-  //             Watch Harry Potter
-  //           </div>
-  //         </div>
 
   function createTodoElement(data) {
+    const $cardText = $("<div>").addClass("card-body").text(data.todo.name); //change this later
+    const $card = $("<div>").addClass("card").append($cardText);
+    return $card;
+  };
 
-    const $cardText = $("<div>").addClass("card-body").text(data.todo.name);
 
+  //assuming data is in an array
+  function renderTodos(data) {
+    for (i = 0; i < data.length; i++) {
+      const todo = createTodoElement(data[i]);
+
+      if (data[i].category === "eat") {
+        $('.eat').append(todo);
+      } else if (data[i].category == "watch") {
+        $('.watch').append(todo);
+      } else if (data[i].category == "read") {
+        $('.read').append(todo);
+      } else if (data[i].category == "buy") {
+        $('.buy').append(todo);
+      }
+
+    }
   }
 
   $.ajax('/todos', { method: 'GET', data: data})
