c9ae7aafe6d895b3b34a2b08b0007ed28bb362a4
Rosy Lee <rosyhlee@gmail.com>
2018-10-19 20:14:18 +0000

    added form capture and AJAX post route

--- datahelpers.js
+++ datahelpers.js
@@ -11,3 +11,19 @@ module.exports = function(knex) {
     }
   };
 }
+
+//send request to Yelp
+
+function requestToYelp(todo, cb) {
+  var options = {
+    url: "https://api.yelp.com/v3/businesses/search?term=" + todo + "&location=canada",
+    headers: {
+      'User-Agent': 'lavieenrosy',
+      //process.env.USER_AGENT,
+      'Authorization': 'token ' + auth.GITHUB_TOKEN
+      //'Authorization': 'token ' + process.env.GITHUB_TOKEN
+    },
+    json: true
+  };
+  request(options, cb);
+}
