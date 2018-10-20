b9289fbb7fc0058383b7d5ad4c54a5d20de96dc6
Gaurav <netgary7@gmail.com>
2018-10-20 02:07:22 +0000

    cleaned up the code and switched branch

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,27 +1,6 @@
 $(() => {
 
 
-  // $.ajax({
-  //   method: "GET",
-  //   url: "/api/users"
-  // }).done((users) => {
-  //   for(user of users) {
-  //     $("<div>").text(user.name).appendTo($("body"));
-  //   }
-  // });;
-
-const actualData = [
-  {id: 1, name: 'Starbucks', category: 'eat', user_id: 1},
-  {id: 2, name: 'Whitespot', category: 'eat', user_id: 2},
-  {id: 3, name: 'life of pi', category: 'read', user_id: 3},
-  {id: 4, name: 'Charlie and the chocolate factory', category:'watch', user_id: 1},
-  {id: 5, name: 'Iphone', category:'buy', user_id: 4},
-  {id: 6, name: 'Nikon', category:'buy', user_id: 3},
-  {id: 7, name: 'Free your mind', category:'read', user_id: 2},
-  {id: 8, name: 'Alibaba: The house Jack Ma built', category:'read', user_id: 4},
-  {id: 9, name: 'Nikon', category:'buy', user_id: 2}
-]
-
   function createTodoElement(data) {
     const $cardText = $("<div>").addClass("card-body").text(data.name); //change this later
     const $card = $("<div>").addClass("card").append($cardText);
@@ -31,7 +10,6 @@ const actualData = [
 
   };
 
-  //assuming data is in an array
   function renderTodos(data) {
     for (i = 0; i < data.length; i++) {
       const todo = createTodoElement(data[i]);
@@ -91,8 +69,6 @@ $( '.col' ).droppable();
 
 $('form').on('submit', function (event) {
   event.preventDefault();
-  // const newTodo = $('.todo-form').find('.form-control').val();
-  // console.log(newTodo);
   const data = $('form').serialize();
 
   //form validation?
